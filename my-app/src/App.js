a303fbb5a201a250bbec7dfec9cf362a04e58614
Jordan Russell <jordanrussell1@gmail.com>
2018-10-24 16:44:13 -0400

    need to adjust app

---
+++ my-app/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Row, CardPanel, Col } from 'react-materialize';
+
+class App extends Component {
+ state = {
+  //need to add icons 
+  icons: [],
+  clicked: [],
+  score: 0,
+  highScore: 0,
+  correct: null,
+  gameWon: false
+}
+
+  randomize = (a, b) => Math.random() > .5 ? -1 : 1;
+
+  clickHandler = iconName => {
+    if (this.state.clicked.indexOf(iconName) === -1) {
+      let score = this.state.clicked.length +1,
+      clicked = score ===this.state.icons.length ? [] : [...this.state.clicked, iconName]
+
+      this.setState({
+        icons: this.state.icons.sort(this.randomize),
+        clicked: clicked,
+        score: score,
+        highScore: Math.max(this,state.highScore, score),
+        correct: true,
+        gameWon: score === this.state.icons.length
+      })
+    } else {
+      this.setState({
+        icons: this.state.icons.sort(this.randomize),
+        clicked: [],
+        score: 0,
+        correct: false,
+        gameWon: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container-fluid">
+          <Nav correct={this.state.correct} gameWon={this.state.gameWon} score={this.state.score} highScore={this.state.highScore} />
+            <Row>
+              <Col s={12}>
+                <CardPanel>
+                  <p>Click any image to increase your points - but don't click it more that once.</p>
+                </CardPanel>
+              </Col>
+            </Row>
+          </div>
+        <div className="container">
+          <Row>
+            {this.state.icons.map(icon => <GamePiece correct={this.state.correct} key={icon.iconName} icon={icon} clickHanler={this.clickHandler />)}
+          </Row>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
