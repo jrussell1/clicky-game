a61e51acd09cbc57eafa5c30231576bdbfa9df7f
Jordan Russell <jordanrussell1@gmail.com>
2018-10-24 22:50:22 -0400

    first placeholder commit

---
+++ my-app/src/components/Placeholder.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Col, CardPanel } from 'react-materialize';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Placeholder = props => ();
+
+export default GamePiece
